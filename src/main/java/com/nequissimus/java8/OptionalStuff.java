295cfb4b3d935aff416a8cc5b23d5f0233e338d8
NeQuissimus <steinbach.tim@googlemail.com>
2014-06-03 16:44:35 -0400

    Use map, instead of flatMap and add 'old' ways of handling it

--- src/main/java/com/nequissimus/java8/OptionalStuff.java
+++ src/main/java/com/nequissimus/java8/OptionalStuff.java
@@ -5,16 +5,20 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 final class OptionalStuff {
+  static int expensiveCall() {
+    try {
+      Thread.sleep(10_000);
+    } catch (InterruptedException e) {}
+
+    return 0;
+  }
+
   /**
   * "Expensive" call to an API, used for {Optional#orElseGet}
   */
   static class Fallback implements Supplier<Integer> {
     public Integer get() {
-      try {
-        Thread.sleep(10_000);
-      } catch (InterruptedException e) {}
-
-      return 0;
+      return expensiveCall();
     }
   }
 
@@ -30,11 +34,55 @@ final class OptionalStuff {
   }
 
   public static void main(String[] args) {
-    final Function<String, Optional<Integer>> len = x -> Optional.of(x.length());
-
     final Optional<String> oStr = Optional.ofNullable(randomString());
-    final Optional<Integer> oLen = oStr.flatMap(len);
+    final Optional<Integer> oLen = oStr.map(x -> x.length());
 
     System.out.println("Length = " + oLen.orElseGet(new Fallback()));
+  }
+
+  /**
+  * Pre-Java 8 with try/catch
+  */
+  public static void oldWayTry() throws Exception {
+    final String r = randomString();
+    int l;
+
+    try {
+      l = r.length();
+    } catch (Exception e) {
+      l = expensiveCall();
+    }
+
+    System.out.println("Length = " + l);
+  }
+
+  /**
+  * Initialize to "null"-value, then replace with proper value
+  */
+  public static void oldWayInit() {
+    final String r = randomString();
+    int l = expensiveCall();
+
+    if (r != null) {
+      l = r.length();
+    }
+
+    System.out.println("Length = " + l);
+  }
+
+  /**
+  * Pre-Java 8 with a guard
+  */
+  public static void oldWayGuard() {
+    final String r = randomString();
+    int l;
+
+    if (r != null) {
+      l = r.length();
+    } else {
+      l = expensiveCall();
+    }
+
+    System.out.println("Length = " + l);
   }
 }
